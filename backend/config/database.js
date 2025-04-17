d8a666fa6ca2bcee2dd92d3ffb8a5e2f2329b3d0
ayrisatrianida <ayrisatrianida04@gmail.com>
2025-04-17 13:32:30 +0700

    database baru

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 
 const db = new Sequelize('RECOVER_YOUR_DATA', 'root' , '',{
-    host : '34.60.177.44',
+    host : '35.222.154.88',
     dialect : 'mysql',
 } );
 
